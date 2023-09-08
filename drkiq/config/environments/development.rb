0d297410411a268730dc32263f2b7282d6dc59dc
Administrator <admin@example.com>
2023-09-08 16:32:20 +0800

    No commit message provided

--- drkiq/config/environments/development.rb
+++ drkiq/config/environments/development.rb
@@ -4,7 +4,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 #  config.hosts << "drkiq"
-config.hosts = ["drkiq","27big.com"]
+config.hosts = ["drkiq","27big.com","127.0.0.1"]
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
